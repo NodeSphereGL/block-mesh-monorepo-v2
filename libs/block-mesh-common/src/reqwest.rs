d0f1c1e186c836a816a5c2cdd81f6a1efd53e514
Toan Pham <toan@betanet.co.il>
2025-02-16 22:46:07 +0700

    revert code

--- libs/block-mesh-common/src/reqwest.rs
+++ libs/block-mesh-common/src/reqwest.rs
@@ -1,36 +1,23 @@
 use crate::constants::DeviceType;
-extern crate reqwest as reqwest_crate;
-use reqwest_crate::{Client, ClientBuilder, Proxy};
+use reqwest::{Client, ClientBuilder};
 #[allow(unused_imports)]
-use std::{env, time::Duration};
+use std::time::Duration;
 
 #[cfg(target_arch = "wasm32")]
 pub fn http_client(device_type: DeviceType) -> Client {
-    let mut client_builder = ClientBuilder::new().user_agent(format!(
-        "curl/8.7.1; {}; {}",
-        device_type,
-        env!("CARGO_PKG_VERSION")
-    ));
-
-    // Add proxy support (SOCKS5)
-    if let Ok(proxy_url) = env::var("SOCKS5_PROXY") {
-        match Proxy::all(&proxy_url) {
-            Ok(proxy) => {
-                client_builder = client_builder.proxy(proxy);
-                println!("Using SOCKS5 proxy: {}", proxy_url); // Add logging
-            }
-            Err(e) => {
-                eprintln!("Failed to create SOCKS5 proxy: {}", e); // Add error logging
-            }
-        }
-    }
-
-    client_builder.build().unwrap_or_default()
+    ClientBuilder::new()
+        .user_agent(format!(
+            "curl/8.7.1; {}; {}",
+            device_type,
+            env!("CARGO_PKG_VERSION")
+        ))
+        .build()
+        .unwrap_or_default()
 }
 
 #[cfg(not(target_arch = "wasm32"))]
 pub fn http_client(device_type: DeviceType) -> Client {
-    let mut client_builder = ClientBuilder::new()
+    ClientBuilder::new()
         .timeout(Duration::from_secs(3))
         .cookie_store(true)
         .user_agent(format!(
@@ -39,20 +26,7 @@ pub fn http_client(device_type: DeviceType) -> Client {
             env!("CARGO_PKG_VERSION")
         ))
         .no_hickory_dns()
-        .use_rustls_tls();
-
-    // Add proxy support (SOCKS5)
-    if let Ok(proxy_url) = env::var("SOCKS5_PROXY") {
-        match Proxy::all(&proxy_url) {
-            Ok(proxy) => {
-                client_builder = client_builder.proxy(proxy);
-                println!("Using SOCKS5 proxy: {}", proxy_url); // Add logging
-            }
-            Err(e) => {
-                eprintln!("Failed to create SOCKS5 proxy: {}", e); // Add error logging
-            }
-        }
-    }
-
-    client_builder.build().unwrap_or_default()
+        .use_rustls_tls()
+        .build()
+        .unwrap_or_default()
 }
